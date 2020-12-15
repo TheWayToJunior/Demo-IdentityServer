3aabb9f03674f1b165e44e342eef91e979c0e519
TheWayToJunior <developermicrosoft1932@gmail.com>
2020-12-15 18:33:27 +0300

    Rename projectДобавьте файлы проекта.

---
+++ DemoIdentityServer.Server/Infrastructure/ProfileService.cs
@@ -0,0 +1,42 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Services;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DemoIdentityServer.Server
+{
+    public class ProfileService : IProfileService
+    {
+        /// <summary>
+        /// This method is called whenever claims about the user are requested (e.g. during token creation or via the userinfo endpoint)
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.DateOfBirth, "01.01.2010")
+            };
+
+            context.IssuedClaims.AddRange(claims);
+
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// This method gets called whenever identity server needs to determine if the user is valid or active (e.g. if the user's account has been deactivated since they logged in).
+        /// (e.g. during token issuance or validation).
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            context.IsActive = true;
+            return Task.CompletedTask;
+        }
+    }
+}
